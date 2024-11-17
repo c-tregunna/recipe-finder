09993ca58cb6d32127e3801522299f7875f9407b
Claire <c.tregunna1980@gmail.com>
2024-11-17 14:36:01 +0000

    hooked up to Tasty api and showing results in console

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const navBars = document.querySelector('.fa-bars ')
 const navOverlay = document.querySelector('.overlay-nav')
 const navClose = document.querySelector('.nav-close')
+const displayRecipes = document.getElementById('display')
 
 
 // Navigation event listeners
@@ -11,3 +12,25 @@ navBars.addEventListener('click', function(){
 navClose.addEventListener('click', function() {
     navOverlay.style.width = "0"
 })
+
+
+// Tasty API https://rapidapi.com/apidojo/api/tasty
+const url = 'https://tasty.p.rapidapi.com/recipes/list?from=0&size=10&tags=chicken';
+const options = {
+	method: 'GET',
+	headers: {
+		'x-rapidapi-key': '37b61ae7ccmshd35c9cd4d587a48p1b476cjsnf552552cf8a3',
+		'x-rapidapi-host': 'tasty.p.rapidapi.com'
+	}
+};
+
+function getRecipe() {
+    return fetch(url, options)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data.results[1].name)
+        return data.results //gives the array, just need to figure out how to get at it
+    })
+}
+
+getRecipe() 
