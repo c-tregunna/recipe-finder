fda53bf75f15ecbb407dd343fbb6522fbae46fe5
Claire <c.tregunna1980@gmail.com>
2024-11-29 16:49:26 +0000

    added most Tailwind classes and alter bar functionality

--- app.js
+++ app.js
@@ -1,10 +1,27 @@
+const alertText = document.getElementById('alert-text')
+const messages = [
+    "New Christmas recipes available now",
+    "looking for party food to see in the New Year? Click here",
+    "Make a homemade treat to give for Christmas"
+]
 
-  tailwind.config = {
-    theme: {
-      extend: {
-        fontFamily: {
-          outfit: ['Outfit', 'sans-serif'],
-        },
-      },
-    },
+function showAlertMessages(messages, displayDuration = 8000) {
+  let currentIndex = 0
+
+  function updateMessage() {
+    alertText.style.opacity ='0'
+
+    setTimeout(() => {
+      alertText.textContent = messages[currentIndex]
+      alertText.style.opacity = '1'
+      currentIndex = (currentIndex + 1) % messages.length
+    }, 1000)
   }
+  updateMessage()
+  setInterval(updateMessage, displayDuration)
+}
+
+window.onload = () => {
+  showAlertMessages(messages)
+}
+
